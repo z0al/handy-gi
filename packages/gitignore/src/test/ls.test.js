f3208a7d869db73fb57a512d39a47c6c6218ab91
Ahmed Tajelsir <ah.tajelsir@gmail.com>
2017-06-19 22:31:06 +0300

    [core] Just small clarification

--- packages/gitignore/src/test/ls.test.js
+++ packages/gitignore/src/test/ls.test.js
@@ -7,7 +7,7 @@ describe('ls', () => {
     const files = await ls(dir)
     expect(files).not.toEqual([])
   })
-  it('excludes given patterns', async () => {
+  it('ignores given patterns', async () => {
     const files = await ls(dir, ['package.json'])
     expect(files.indexOf('node_modules/jest')).toEqual(-1)
     expect(files.indexOf('package.json')).toEqual(-1)
