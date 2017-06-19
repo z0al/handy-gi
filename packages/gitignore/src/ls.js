058d79f4276dd2edcc924ae0f9c3b73c9754b71d
Ahmed Tajelsir <ah.tajelsir@gmail.com>
2017-06-19 23:14:28 +0300

    [core] ls: use 'cmd' option of glob instead of our hack

--- packages/gitignore/src/ls.js
+++ packages/gitignore/src/ls.js
@@ -3,16 +3,9 @@ import { exclude } from './config.json'
 
 export default async (dir, ignore = []) => {
   return new Promise(resolve => {
-    // A hack for applying glob only under 'dir'
-    const cwd = process.cwd()
-    process.chdir(dir)
-
     glob('**',
-      {ignore: [...ignore, ...exclude], follow: false},
+      {ignore: [...ignore, ...exclude], cwd: dir, follow: false},
       (err, files) => {
-        // Revert the change we made in cwd earlier
-        process.chdir(cwd)
-
         files = err ? null : files
         resolve(files)
       }
