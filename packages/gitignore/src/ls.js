6956283a7b291873c4c67de9943b57c988aecafe
Ahmed Tajelsir <ah.tajelsir@gmail.com>
2017-06-19 23:11:36 +0300

    [core] ls: work only inside given directory

--- packages/gitignore/src/ls.js
+++ packages/gitignore/src/ls.js
@@ -3,9 +3,16 @@ import { exclude } from './config.json'
 
 export default async (dir, ignore = []) => {
   return new Promise(resolve => {
+    // A hack for applying glob only under 'dir'
+    const cwd = process.cwd()
+    process.chdir(dir)
+
     glob('**',
-      {ignore: [...ignore, ...exclude], root: dir, follow: false},
+      {ignore: [...ignore, ...exclude], follow: false},
       (err, files) => {
+        // Revert the change we made in cwd earlier
+        process.chdir(cwd)
+
         files = err ? null : files
         resolve(files)
       }
