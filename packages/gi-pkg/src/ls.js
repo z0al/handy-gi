5d47fbdd5768461e57f3ee9bb6748decc14bf736
Ahmed Tajelsir <ah.tajelsir@gmail.com>
2017-06-20 13:11:46 +0300

    [pkg] ls: use wildcards

--- packages/gi-pkg/src/ls.js
+++ packages/gi-pkg/src/ls.js
@@ -3,7 +3,7 @@ import { exclude } from './config.json'
 
 export default async (dir, ignore = []) => {
   return new Promise(resolve => {
-    glob('**',
+    glob('*',
       {ignore: [...ignore, ...exclude], cwd: dir, follow: false},
       (err, files) => {
         files = err ? null : files
