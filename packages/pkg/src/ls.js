b2b5167506c2d2538a768ebd25f948dae13924d7
Ahmed Tajelsir <ah.tajelsir@gmail.com>
2017-06-22 17:58:20 +0300

    [pkg] some docs for 'ls' module

--- packages/pkg/src/ls.js
+++ packages/pkg/src/ls.js
@@ -1,11 +1,41 @@
 import glob from 'glob'
-import { exclude } from './config.json'
+import { exclude as exclusionList } from './config.json'
 
-const ls = async (dir, ignore = []) => {
+/**
+ * Asynchronous glob-based directory listing
+ *
+ * NOTE: For better performance we implicitly exclude the **content** of these
+ * folders:
+ *
+ *  - `node_modules`
+ *  - `.git`
+ *  - `.svn`
+ *
+ * However, you would still gets their names listed, e.g: node_modules,
+ * which enables us to perform matching checks as neccessary.
+ *
+ * Also, it doesn't follow symbolic links!
+ *
+ * @example
+ * var files = await ls('path/to/my/node/module')
+ * // outputs(e.g): ['package.json','src/index.js', ...]
+ *
+ * var files = await ls('path/to/my/node/module', ['package.json'])
+ * // outputs(e.g): ['src/index.js', ...]
+ *
+ * @param  {String}  dir            target directory
+ * @param  {Array}   [exclude=[]]   of glob patterns to exclude
+ * @return {Promise}                with folders/files list in `dir`
+ */
+const ls = async (dir, exclude = []) => {
   return new Promise((resolve, reject) => {
-    glob('*',
-      {ignore: [...ignore, ...exclude], cwd: dir, follow: false},
-      (err, files) => {
+    glob(
+      '*', // matches anything
+      {
+        ignore: [...exclude, ...exclusionList],
+        cwd: dir,
+        follow: false // don't follow symbolic links
+      }, (err, files) => {
         if (err) {
           reject(err)
         } else {
